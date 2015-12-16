c21c0af9cdb29ae9e2f70ebdef59a05081ae86ef
Jorgo Miridis <jorgo@miridis.com>
2015-12-16 13:52:07 -0500

    add pseudo-pagination to backend

--- apps/backend/modules/reservation/actions/actions.class.php
+++ apps/backend/modules/reservation/actions/actions.class.php
@@ -10,68 +10,75 @@
  */
 class reservationActions extends sfActions
 {
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->reservations = Doctrine_Core::getTable('Reservation')
-      ->createQuery('a')
-      ->orderBy('a.arrival_date DESC')
-      ->execute();
-  }
-
-  public function executeNew(sfWebRequest $request)
-  {
-    $this->form = new ReservationForm();
-  }
-
-  public function executeCreate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST));
-
-    $this->form = new ReservationForm();
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('new');
-  }
-
-  public function executeShow(sfWebRequest $request)
-  {
-    $this->forward404Unless($this->reservation = Doctrine_Core::getTable('Reservation')->find(array($request->getParameter('id'))), sprintf('Object reservation does not exist (%s).', $request->getParameter('id')));
-
-    $this->transactions = $this->reservation->PaypalTransactions;
-
-    $this->reservationUrl = Encryption::getEncryptedUrlFromUri('@reservation_show?uniqid='.$this->reservation->uniqid, $this->reservation->Client->User->id, false);
-  }
-
-  public function executeUpdate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
-    $this->forward404Unless($reservation = Doctrine_Core::getTable('Reservation')->find(array($request->getParameter('id'))), sprintf('Object reservation does not exist (%s).', $request->getParameter('id')));
-    $this->form = new ReservationForm($reservation);
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('edit');
-  }
-
-  public function executeDelete(sfWebRequest $request)
-  {
-    $request->checkCSRFProtection();
-
-    $this->forward404Unless($reservation = Doctrine_Core::getTable('Reservation')->find(array($request->getParameter('id'))), sprintf('Object reservation does not exist (%s).', $request->getParameter('id')));
-    $reservation->delete();
-
-    $this->redirect('reservation/index');
-  }
-
-  protected function processForm(sfWebRequest $request, sfForm $form)
-  {
-    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-    if ($form->isValid())
+    public function executeIndex(sfWebRequest $request)
     {
-      $reservation = $form->save();
+        $pageNumber = (int)$request->getGetParameter('page');
 
-      $this->redirect('reservation/edit?id='.$reservation->getId());
+        $limit = 100;
+        $offset = $limit * ($pageNumber > 1)? $pageNumber - 1 : $pageNumber;
+
+        $this->reservations = Doctrine_Core::getTable('Reservation')
+            ->createQuery('a')
+            ->orderBy('a.arrival_date DESC')
+            ->limit($limit)
+            ->offset($offset)
+            ->execute();
     }
-  }
+
+    public function executeNew(sfWebRequest $request)
+    {
+        $this->form = new ReservationForm();
+    }
+
+    public function executeCreate(sfWebRequest $request)
+    {
+        $this->forward404Unless($request->isMethod(sfRequest::POST));
+
+        $this->form = new ReservationForm();
+
+        $this->processForm($request, $this->form);
+
+        $this->setTemplate('new');
+    }
+
+    public function executeShow(sfWebRequest $request)
+    {
+        $this->forward404Unless($this->reservation = Doctrine_Core::getTable('Reservation')->find(array($request->getParameter('id'))), sprintf('Object reservation does not exist (%s).', $request->getParameter('id')));
+
+        $this->transactions = $this->reservation->PaypalTransactions;
+
+        $this->reservationUrl = Encryption::getEncryptedUrlFromUri('@reservation_show?uniqid='.$this->reservation->uniqid, $this->reservation->Client->User->id, false);
+    }
+
+    public function executeUpdate(sfWebRequest $request)
+    {
+        $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
+        $this->forward404Unless($reservation = Doctrine_Core::getTable('Reservation')->find(array($request->getParameter('id'))), sprintf('Object reservation does not exist (%s).', $request->getParameter('id')));
+        $this->form = new ReservationForm($reservation);
+
+        $this->processForm($request, $this->form);
+
+        $this->setTemplate('edit');
+    }
+
+    public function executeDelete(sfWebRequest $request)
+    {
+        $request->checkCSRFProtection();
+
+        $this->forward404Unless($reservation = Doctrine_Core::getTable('Reservation')->find(array($request->getParameter('id'))), sprintf('Object reservation does not exist (%s).', $request->getParameter('id')));
+        $reservation->delete();
+
+        $this->redirect('reservation/index');
+    }
+
+    protected function processForm(sfWebRequest $request, sfForm $form)
+    {
+        $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
+        if ($form->isValid())
+        {
+            $reservation = $form->save();
+
+            $this->redirect('reservation/edit?id='.$reservation->getId());
+        }
+    }
 }
